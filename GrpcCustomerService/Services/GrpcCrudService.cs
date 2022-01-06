28c747bd680afd8dc42d738ad96cacaba0ad4105
russuvlad97 <russu.vlad97@gmail.com>
2022-01-06 17:50:18 +0200

    Added lab12

--- GrpcCustomerService/Services/GrpcCrudService.cs
+++ GrpcCustomerService/Services/GrpcCrudService.cs
@@ -19,6 +19,20 @@ namespace GrpcCustomerService
             this.db = db;
         }
 
+        public override Task<Customer> Get (CustomerId requestData, ServerCallContext context)
+         {
+            var data = db.Customers.Find(requestData.Id);
+
+            Customer emp = new Customer()
+            {
+                CustomerId = data.CustomerID,
+                Name = data.Name,
+                Address = data.Address
+            };
+
+            return Task.FromResult(emp);
+         }
+
         public override Task<CustomerList> GetAll(Empty empty, ServerCallContext context)
         {
 
@@ -28,21 +42,46 @@ namespace GrpcCustomerService
                         {
                             CustomerId = cust.CustomerID,
                             Name = cust.Name,
-                            Adress = cust.Address
+                            Address = cust.Address
                         };
             pl.Item.AddRange(query.ToArray());
             return Task.FromResult(pl);
         }
+
         public override Task<Empty> Insert(Customer requestData, ServerCallContext context)
         {
             db.Customers.Add(new ModelAccess.Customer
             {
                 CustomerID = requestData.CustomerId,
                 Name = requestData.Name,
-                Address = requestData.Adress,
+                Address = requestData.Address,
                 BirthDate = DateTime.Parse(requestData.Birthdate)
             });
             db.SaveChanges();
+            return Task.FromResult(new Empty());
+        }
+
+        public override Task<Empty> Delete(CustomerId requestData, ServerCallContext context)
+        {
+            var data = db.Customers.Find(requestData.Id);
+
+            db.Customers.Remove(data);
+            db.SaveChanges();
+
+            return Task.FromResult(new Empty());
+        }
+
+        public override Task<Empty> Update(Customer requestData, ServerCallContext context)
+        {
+            db.Customers.Update(new ModelAccess.Customer()
+                {
+                CustomerID = requestData.CustomerId,
+                Name = requestData.Name,
+                Adress = requestData.Adress,
+                BirthDate = DateTime.Parse(requestData.Birthdate)
+                });
+            db.SaveChanges();
+
             return Task.FromResult(new Empty());
         }
     }
